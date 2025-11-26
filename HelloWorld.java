4f96b683ebbb7edd64ef0eb5d0e14ca7675ea118
egealtuner <egealtuner3@icloud.com>
2025-11-26 14:51:30 +0300

    lab6
    lab 6

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+// scenario 0- lab 6
+// a)6 b)4 c)5 d)15 e)16 f)cause the program will stop running
+        public class HelloWorld {
+            public static void main(String[] args){
+                int[] myArr = {4, 8, 15, 16, 23, 42};
+                System.out.println("a. Length of myArr:" + myArr.length);
+                System.out.println("b. value of the first element (myArr[0]]:" + myArr[0]);
+                System.out.println("c.. index of the last element:"+ (myArr.length-1));
+                System.out.println("d.value of the third element (myArr[2]):"+ myArr[2]);
+                System.out.println("e.value of myArr[3]:" + myArr[3]);
+                System.out.println("if we uncomment the next line, the program will crash");
+    }
+}
